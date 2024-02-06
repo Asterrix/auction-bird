47e4bd24341592294b55ad310faefbcec04a9f8d
Asterrix <nezirevic.enis@outlook.com>
2024-02-06 23:59:50 +0100

    feature(navbar-footer): create footer component

--- Presentation/ClientApp/src/App.tsx
+++ Presentation/ClientApp/src/App.tsx
@@ -1,12 +1,14 @@
 import {ROUTER} from "./pages/routing.ts";
 import {RouterProvider} from "react-router-dom";
 import {NavbarComponent} from "./components/navbar/navbar.component.tsx";
+import {FooterComponent} from "./components/footer/footer.component.tsx";
 
 export default function App() {
   return (
     <>
       <NavbarComponent/>
       <RouterProvider router={ROUTER}/>
+      <FooterComponent/>
     </>
   );
 }
