cdb395f250e5565aba284ceb8efbdb496347c87d
Asterrix <nezirevic.enis@outlook.com>
2024-02-07 00:53:47 +0100

    feature(structure): change config file to use docker image only

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,7 +1,6 @@
 ﻿using Infrastructure.Caching;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -24,35 +23,10 @@ public static class DependencyInjection
 
     private static void ConfigureDatabaseConnection(this IServiceCollection serviceCollection)
     {
-        IConfigurationRoot config;
-
-        try
-        {
-            // This is for local development
-            string basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Presentation", "env");
-            config = new ConfigurationBuilder()
-                .SetBasePath(basePath)
-                .AddJsonFile("postgres.json")
-                .AddEnvironmentVariables()
-                .Build();
-        }
-
-        // If the above fails, then we are in a docker container
-        catch (Exception)
-        {
-            // This is for docker container environment
-            const string basePath = "/app/env";
-            config = new ConfigurationBuilder()
-                .SetBasePath(basePath)
-                .AddJsonFile("postgres.json")
-                .AddEnvironmentVariables()
-                .Build();
-        }
-
         serviceCollection.AddDbContext<DatabaseContext>(options =>
         {
             options
-                .UseNpgsql(config.GetConnectionString("DefaultConnection"))
+                .UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING"))
                 .UseSnakeCaseNamingConvention();
         });
     }
