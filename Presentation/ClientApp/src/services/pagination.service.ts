3500e88105368778dca59f4090d641e34d350509
Asterrix <nezirevic.enis@outlook.com>
2024-02-08 18:08:15 +0100

    feature(marketplace-page): connect items api to the client - create pagination service

---
+++ Presentation/ClientApp/src/services/pagination.service.ts
@@ -0,0 +1,21 @@
+﻿import {Pageable} from "../utils/types/pagination/pageable.type";
+import {Page} from "../utils/types/pagination/page.type";
+
+const defaultPagination: () => Pageable = () => ({
+  page: 1,
+  size: 9
+});
+
+const nextPage = (pageable: Pageable, page: Page<any>): Pageable => {
+  if (!page.isLastPage) {
+    return {page: pageable.page + 1, size: pageable.size};
+  }
+
+  return pageable;
+};
+
+const resetPagination = (): Pageable => {
+  return defaultPagination();
+};
+
+export {defaultPagination, nextPage, resetPagination};
