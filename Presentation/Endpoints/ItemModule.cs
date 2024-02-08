d338f2466853efbc0b08bce3ed839f838d67f8ac
Asterrix <nezirevic.enis@outlook.com>
2024-02-08 17:17:31 +0100

    feature(marketplace-page): change Page<T> class - create two constructors, each serve different purpose (one for returning Page<T> object from Repository, the other for returning Page<T> object from Cache) - implement JsonConverter for Page<T> object

--- Presentation/Endpoints/ItemModule.cs
+++ Presentation/Endpoints/ItemModule.cs
@@ -6,6 +6,7 @@ using Infrastructure.Caching;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Presentation.JsonConverters;
 
 namespace Presentation.Endpoints;
 
@@ -23,23 +24,21 @@ public sealed class ItemModule() : CarterModule(Versioning.Version)
         [FromQuery] int pageSize)
     {
         string cacheKey = $"items_list_{pageNumber}_{pageSize}";
-        DistributedCacheEntryOptions cacheOptions = new CacheOptionsBuilder()
-            .WithKey(cacheKey)
-            .WithAbsoluteExpirationRelativeToNow(TimeSpan.FromMinutes(5))
-            .Build();
-        
-        Page<ItemSummary>? cachedItems = await cachingService.GetAsync<Page<ItemSummary>>(cacheKey);
+
+        Page<ItemSummary>? cachedItems = await cachingService.GetAsync<Page<ItemSummary>>(cacheKey, PageConverter<ItemSummary>.GetSettings());
         if (cachedItems is not null)
         {
-            // Because the cached item does not contain the reference to Pageable object,
-            // we need to create a new one in order for properties TotalPages and IsLastPage to be calculated correctly.
-            // Else NullReferenceException will be thrown when trying to access these properties.
-            cachedItems = new Page<ItemSummary>(cachedItems.Elements, Pageable.Of(pageNumber, pageSize), cachedItems.TotalElements);
             return Results.Ok(cachedItems);
         }
 
         Pageable pageable = Pageable.Of(pageNumber, pageSize);
         Page<ItemSummary> items = await sender.Send(new ListItemsQuery(pageable));
+
+        DistributedCacheEntryOptions cacheOptions = new CacheOptionsBuilder()
+            .WithKey(cacheKey)
+            .WithAbsoluteExpirationRelativeToNow(items.IsLastPage ? TimeSpan.FromMinutes(1) : TimeSpan.FromMinutes(10))
+            .Build();
+
         await cachingService.SetAsync(cacheKey, items, cacheOptions);
 
         return Results.Ok(items);
