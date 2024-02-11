8b5cce7a0f79d9c41d6479773c4a68451aa6d69b
Asterrix <nezirevic.enis@outlook.com>
2024-02-11 17:28:54 +0100

    feature(marketplace-page): create specification abstraction to be used for filtration

---
+++ Application/Specification/Specification.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Specification;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>> AsExpression();
+}
+
+public sealed class Specification<T>
+{
+    public Expression<Func<T, bool>> SpecificationExpression { get; private set; } = _ => true;
+
+    private Specification()
+    {
+    }
+
+    public static Specification<T> Create()
+    {
+        return new Specification<T>();
+    }
+
+    public Specification<T> And(Expression<Func<T, bool>> expression)
+    {
+        SpecificationExpression = Expression.Lambda<Func<T, bool>>(
+            Expression.AndAlso(
+                SpecificationExpression.Body,
+                Expression.Invoke(expression, SpecificationExpression.Parameters)
+            ),
+            SpecificationExpression.Parameters
+        );
+        return this;
+    }
+
+    public Specification<T> AndNot(Expression<Func<T, bool>> expression)
+    {
+        SpecificationExpression = Expression.Lambda<Func<T, bool>>(
+            Expression.AndAlso(
+                SpecificationExpression.Body,
+                Expression.Not(Expression.Invoke(expression, SpecificationExpression.Parameters))
+            ),
+            SpecificationExpression.Parameters
+        );
+        return this;
+    }
+
+    public Specification<T> Or(Expression<Func<T, bool>> expression)
+    {
+        SpecificationExpression = Expression.Lambda<Func<T, bool>>(
+            Expression.OrElse(
+                SpecificationExpression.Body,
+                Expression.Invoke(expression, SpecificationExpression.Parameters)
+            ),
+            SpecificationExpression.Parameters
+        );
+        return this;
+    }
+
+    public Specification<T> OrNot(Expression<Func<T, bool>> expression)
+    {
+        SpecificationExpression = Expression.Lambda<Func<T, bool>>(
+            Expression.OrElse(
+                SpecificationExpression.Body,
+                Expression.Not(Expression.Invoke(expression, SpecificationExpression.Parameters))
+            ),
+            SpecificationExpression.Parameters
+        );
+        return this;
+    }
+
+    public Specification<T> Not()
+    {
+        SpecificationExpression = Expression.Lambda<Func<T, bool>>(
+            Expression.Not(SpecificationExpression.Body),
+            SpecificationExpression.Parameters
+        );
+        return this;
+    }
+}
