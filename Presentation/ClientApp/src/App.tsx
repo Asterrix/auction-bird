a2f542cfbd7923e6e5ee35d21411e24b8eaee858
Asterrix <nezirevic.enis@outlook.com>
2024-02-11 20:18:17 +0100

    feature(marketplace-page): implement search on the client

--- Presentation/ClientApp/src/App.tsx
+++ Presentation/ClientApp/src/App.tsx
@@ -2,12 +2,15 @@ import {ROUTER} from "./pages/routing.ts";
 import {RouterProvider} from "react-router-dom";
 import {NavbarComponent} from "./components/navbar/navbar.component.tsx";
 import {FooterComponent} from "./components/footer/footer.component.tsx";
+import {SearchProvider} from "./components/searchbar/search.provider.tsx";
 
 export default function App() {
   return (
     <>
-      <NavbarComponent/>
-      <RouterProvider router={ROUTER}/>
+      <SearchProvider>
+        <NavbarComponent/>
+        <RouterProvider router={ROUTER}/>
+      </SearchProvider>
       <FooterComponent/>
     </>
   );
