964acae813c21d24cb59a154d024e6ebfc6998ac
Asterrix <122569557+Asterrix@users.noreply.github.com>
2024-02-29 22:10:40 +0100

    feature(structure): remove environment files from react project

--- Presentation/ClientApp/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-﻿export const environment = {
-  production: true,
-  apiUrl: "http://localhost:8080/api/v1", // Development url
-  clientId: ""
-};
--- Presentation/ClientApp/src/environments/environments.development.ts
+++
@@ -1,4 +0,0 @@
-﻿export const environment = {
-  production: false,
-  apiUrl: "http://localhost:8080/api/v1",
-};
